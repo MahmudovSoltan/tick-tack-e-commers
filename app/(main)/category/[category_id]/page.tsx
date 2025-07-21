86ed37498e4c7fbff8eb5b86f28fb22d090508a4
Soltan <mahmudovsoltan477@gmail.com>
2025-07-21 11:12:23 +0400

    fix: remove commented-out code and simplify CategoryDetail component

--- app/(main)/category/[category_id]/page.tsx
+++ app/(main)/category/[category_id]/page.tsx
@@ -1,34 +1,48 @@
-// app/(main)/category/[category_id]/page.tsx
+// // app/(main)/category/[category_id]/page.tsx
 
 
-import CategoriesBody from "@/app/components/categories/CategoriesBody";
-import Header from "../../header";
-import '../css/category.css';
-import LinkComponent from "@/app/ui/links";
+// import CategoriesBody from "@/app/components/categories/CategoriesBody";
+// import Header from "../../header";
+// import '../css/category.css';
+// import LinkComponent from "@/app/ui/links";
 
-interface PageProps {
-  params: {
-    category_id: string;
-  };
-}
+// interface PageProps {
+//   params: {
+//     category_id: string;
+//   };
+// }
 
-// 🔧 Bura diqqət: async əlavə olunub
-export default async function CategoryDetailPage({ params }: PageProps) {
-  const categoryId = Array.isArray(params.category_id)
-    ? params.category_id[0]
-    : params.category_id;
+// // 🔧 Bura diqqət: async əlavə olunub
+// export default async function CategoryDetailPage({ params }: PageProps) {
+//   const categoryId = Array.isArray(params.category_id)
+//     ? params.category_id[0]
+//     : params.category_id;
 
+//   return (
+//     <div>
+//       <div className="container mx-auto">
+//         <Header role="" />
+//       </div>
+//       <section className="categories_container">
+//         <div className="container mx-auto">
+//           <LinkComponent title="Meyvələr" />
+//           <CategoriesBody id={categoryId} />
+//         </div>
+//       </section>
+//     </div>
+//   );
+// }
+
+
+
+import React from 'react'
+
+const CategoryDetail = () => {
   return (
     <div>
-      <div className="container mx-auto">
-        <Header role="" />
-      </div>
-      <section className="categories_container">
-        <div className="container mx-auto">
-          <LinkComponent title="Meyvələr" />
-          <CategoriesBody id={categoryId} />
-        </div>
-      </section>
+      CategoryDetail
     </div>
-  );
+  )
 }
+
+export default CategoryDetail
