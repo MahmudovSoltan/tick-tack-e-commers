6c6f8dc65cf50465098bb9cc8befc062c7469dbc
Soltan <mahmudovsoltan477@gmail.com>
2025-07-21 11:06:12 +0400

    refactor: simplify product detail page by removing unused components and props

--- app/(main)/product/[product_id]/page.tsx
+++ app/(main)/product/[product_id]/page.tsx
@@ -1,31 +1,9 @@
-// app/(main)/product/[product_id]/page.tsx
+import React from 'react'
 
-
-import LinkComponent from '@/app/ui/links';
-import Header from '../../header';
-import '../css/product.css';
-import ProductBody from '@/app/components/product';
-
-interface IPageProps {
-  params: {
-    product_id: string;
-  };
-}
-
-export default async function Page({ params }: IPageProps) {
-  const { product_id } = params;
-
+const ProductDetail = () => {
   return (
-    <div>
-      <div className="container mx-auto">
-        <Header role="" />
-      </div>
-      <section className="product_container">
-        <div className="container mx-auto">
-          <LinkComponent title="Meyvələr" />
-          <ProductBody id={product_id} />
-        </div>
-      </section>
-    </div>
-  );
+    <div>ProductDetail</div>
+  )
 }
+
+export default ProductDetail
