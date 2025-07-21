3d52c3c9db7fa69c05f2742fb1a1593e9a50b403
Soltan <mahmudovsoltan477@gmail.com>
2025-07-21 11:08:11 +0400

    refactor: restructure product detail page to use async function and improve component organization

--- app/(main)/product/[product_id]/page.tsx
+++ app/(main)/product/[product_id]/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+// app/(main)/product/[product_id]/page.tsx
 
-const ProductDetail = () => {
-  return (
-    <div>ProductDetail</div>
-  )
+
+import LinkComponent from '@/app/ui/links';
+import Header from '../../header';
+import '../css/product.css';
+import ProductBody from '@/app/components/product';
+
+interface IPageProps {
+  params: {
+    product_id: string;
+  };
 }
 
-export default ProductDetail
+export default async function Page({ params }: IPageProps) {
+  const { product_id } = params;
+
+  return (
+    <div>
+      <div className="container mx-auto">
+        <Header role="" />
+      </div>
+      <section className="product_container">
+        <div className="container mx-auto">
+          <LinkComponent title="Meyvələr" />
+          <ProductBody id={product_id} />
+        </div>
+      </section>
+    </div>
+  );
+}
