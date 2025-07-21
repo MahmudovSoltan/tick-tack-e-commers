9ef7bd45b82bed707fb8d724c77cc49ed7513c07
Soltan <mahmudovsoltan477@gmail.com>
2025-07-21 11:09:31 +0400

    fix: refactor product page component to use ProductDetail and remove unused imports

--- app/(main)/product/[product_id]/page.tsx
+++ app/(main)/product/[product_id]/page.tsx
@@ -1,31 +1,42 @@
-// app/(main)/product/[product_id]/page.tsx
+// // app/(main)/product/[product_id]/page.tsx
 
 
-import LinkComponent from '@/app/ui/links';
-import Header from '../../header';
-import '../css/product.css';
-import ProductBody from '@/app/components/product';
+// import LinkComponent from '@/app/ui/links';
+// import Header from '../../header';
+// import '../css/product.css';
+// import ProductBody from '@/app/components/product';
 
-interface IPageProps {
-  params: {
-    product_id: string;
-  };
-}
+// interface IPageProps {
+//   params: {
+//     product_id: string;
+//   };
+// }
 
-export default async function Page({ params }: IPageProps) {
-  const { product_id } = params;
+// export default async function Page({ params }: IPageProps) {
+//   const { product_id } = params;
 
+//   return (
+//     <div>
+//       <div className="container mx-auto">
+//         <Header role="" />
+//       </div>
+//       <section className="product_container">
+//         <div className="container mx-auto">
+//           <LinkComponent title="Meyvələr" />
+//           <ProductBody id={product_id} />
+//         </div>
+//       </section>
+//     </div>
+//   );
+// }
+
+
+
+
+const ProductDetail = () => {
   return (
-    <div>
-      <div className="container mx-auto">
-        <Header role="" />
-      </div>
-      <section className="product_container">
-        <div className="container mx-auto">
-          <LinkComponent title="Meyvələr" />
-          <ProductBody id={product_id} />
-        </div>
-      </section>
-    </div>
-  );
+    <div>ProductDetail</div>
+  )
 }
+
+export default ProductDetail
